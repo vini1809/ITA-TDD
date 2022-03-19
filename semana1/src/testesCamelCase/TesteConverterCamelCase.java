314db7676bcbbbd0a33c46099053eef6e72770c2
Vinicius Oruam <violetoru@ymail.com>
2022-03-18 22:35:44 -0300

    criando classe de testes

---
+++ semana1/src/testesCamelCase/TesteConverterCamelCase.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testesCamelCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author exact
+ *
+ */
+class TesteConverterCamelCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
