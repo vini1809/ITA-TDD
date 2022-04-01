6623dccd33250c76688ee8db7fb2b777c9d10f5a
Vinicius Oruam <violetoru@ymail.com>
2022-04-01 14:41:44 -0300

    criado classe Caixa Eletronico

--- Semana 3 - Sistema Caixa Eletronico/src/banco/CaixaEletronico.java
+++ Semana 3 - Sistema Caixa Eletronico/src/banco/CaixaEletronico.java
@@ -3,5 +3,58 @@ package banco;
 public class CaixaEletronico {
 	
 	private String logContaCorrente;
+IHardware hardware;
+	
+	public String logar(String numCC) throws ProblemaEncontradoCCException, HardwareException{
+		hardware.pegarNumeroDaContaCartao(numCC);
+		setLogContaCorrente(numCC);
+		return "Usuário Autenticado";
+	}
+	
+	public String sacar(BigDecimal vlrSacar) throws ProblemaEncontradoCCException, SemSaldoException, HardwareException{
+		ContaCorrente contaCorrente = hardware.getServicoRemoto().recuperarConta(getLogContaCorrente());
+		BigDecimal saldo = contaCorrente.getSaldo();
+		if(saldo.compareTo(vlrSacar) > 0)
+		{
+			contaCorrente.setSaldo(saldo.subtract(vlrSacar));
+			hardware.entregarDinheiro();
+			hardware.getServicoRemoto().persistirConta(contaCorrente);
+			return "Retire seu dinheiro";
+		}
+		else
+		{
+			throw new SemSaldoException("Saldo insuficiente");
+		}
+	}
+	
+	public String depositar(BigDecimal vlrDeposito) throws ProblemaEncontradoCCException, HardwareException {
+		ContaCorrente contaCorrente = hardware.getServicoRemoto().recuperarConta(getLogContaCorrente());
+		BigDecimal saldo = contaCorrente.getSaldo();
+		hardware.lerEnvelope();
+		contaCorrente.setSaldo(saldo.add(vlrDeposito));
+		hardware.getServicoRemoto().persistirConta(contaCorrente);
+		return "Depósito recebido com sucesso";
+	}
+	
+	public String saldo() throws ProblemaEncontradoCCException, HardwareException{
+		ContaCorrente contaCorrente = hardware.getServicoRemoto().recuperarConta(getLogContaCorrente());
+		BigDecimal saldo = contaCorrente.getSaldo();
+		
+		return "O saldo é R$" + saldo.toString();
+	}
+
+	public String getLogContaCorrente() {
+		return logContaCorrente;
+	}
+
+	public void setLogContaCorrente(String logContaCorrente) {
+		this.logContaCorrente = logContaCorrente;
+	}
+	
+	public CaixaEletronico(MockHardware mockHardware){
+		hardware = mockHardware;
+	}
+
+}
 
 }
